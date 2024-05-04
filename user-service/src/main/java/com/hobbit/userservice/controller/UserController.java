bed7cc66e6fa6439d4bfbb3aa2b957b6305b928c
PaoloBonicco7 <paolo.bonicco1@gmail.com>
2024-05-04 18:57:26 +0200

    update user controller - "cross origin" annotation

--- user-service/src/main/java/com/hobbit/userservice/controller/UserController.java
+++ user-service/src/main/java/com/hobbit/userservice/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
