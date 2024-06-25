5107c6a412136509b8133610ce92653e1d86070b
Davide <davise.mauri998@hotmail.it>
2024-06-25 18:44:09 +0200

    hobbyCardService v1, metodi crud base + ricerca per user e category

---
+++ hobbycard-service/src/main/java/com/hobbit/hobbyCardService/HobbycardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hobbit.hobbyCardService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbycardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbycardServiceApplication.class, args);
+	}
+
+}
